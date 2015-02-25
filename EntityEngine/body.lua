9b4f15fb8d1272d9f7d753a9ff3144f32ec5cfcf
Canadadry <moureyjerome-spam@yahoo.fr>
2015-02-25 22:13:02 +0100

    Correction on collision detection function

--- EntityEngine/body.lua
+++ EntityEngine/body.lua
@@ -8,7 +8,7 @@ function Body:init(param)
     self.x = param.x or 0
     self.y = param.y or 0
     self.angle = param.angle or param.rotation or param.r or 0
-    self.radius = param.raduis or param.size or param.s or 0
+    self.radius = param.radius or param.size or param.s or 0
 end
 
 function Body:testCollision(entity)
@@ -17,7 +17,8 @@ function Body:testCollision(entity)
     local dx = self.x - entity.body.x;
     local dy = self.y - entity.body.y;
     
-    return ((dx * dx) + (dy * dy)) <= (self.radius + entity.body.radius)
+    return ((dx * dx) + (dy * dy)) <= (self.radius + entity.body.radius)*(self.radius + entity.body.radius)
+    
     
 end
 
