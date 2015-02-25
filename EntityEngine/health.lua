bc2c44a7bfe643bdb971d7f4da606be865e1c6d3
Canadadry <moureyjerome-spam@yahoo.fr>
2015-02-25 21:13:24 +0100

    Add invulnerability frame

--- EntityEngine/health.lua
+++ EntityEngine/health.lua
@@ -6,15 +6,20 @@ Health = class()
 function Health:init(param)
     self.entity = param.entity
     self.life = param.life or 1
+    self.recover = param.recover or 0
+    self.lastTimeHit = self.recover
+    self.invicible = self.lastTimeHit < self.recover
 end
 
 function Health:hit(damage)
-  print("bite")
+  if self.lastTimeHit > self.recover then
+    self.lastTimeHit = 0 
     self.life = self.life - damage;
-    if self.life <= 0 then
-        self.entity.isDead = true
-    end
+    if self.life <= 0 then self.entity.isDead = true end
+  end
 end
 
 function Health:update(dt)
+  self.invicible = self.lastTimeHit < self.recover
+  self.lastTimeHit = self.lastTimeHit + dt
 end
